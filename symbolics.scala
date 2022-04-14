a80a0a3edce1ba9504d2030eaaba2587d1fd4b2e
Adam Tillou <qaiviq@gmail.com>
2022-04-14 08:26:39 -0400

    Refactor algebraic classes to use SymInts instead of normal Ints

--- symbolics.scala
+++ symbolics.scala
@@ -1,13 +1,15 @@
-implicit class SymMultiset[T](val m: Map[T, Int]) {
+import scala.util.chaining._
+
+implicit class SymMultiset[T](val m: Map[T, SymInt]) {
   override def toString = m.toList.sortWith(_._2 < _._2)
     .map{t => f"${t._2}(${t._1})"}.mkString(", ")
 
   def ==(o: SymMultiset[T]): Boolean = m == o.m
 
-  def zipNums(o: SymMultiset[T], f: (Int, Int) => Int): SymMultiset[T] =
+  def zipNums(o: SymMultiset[T], f: (SymInt, SymInt) => SymInt): SymMultiset[T] =
     SymMultiset((this.m.keys ++ o.m.keys).toList
-      .map{k => (k, f(this.m.getOrElse(k, 0), o.m.getOrElse(k, 0)))}
-      .filter(_._2 != 0)
+      .map{k => (k, f(this.m.getOrElse(k, SymInt(0)), o.m.getOrElse(k, SymInt(0))))}
+      .filter(_._2 != SymInt(0))
       .toMap)
 
   def ++(o: SymMultiset[T]) = this.zipNums(o, _ + _)
@@ -20,43 +22,56 @@ implicit class SymMultiset[T](val m: Map[T, Int]) {
   def toList = m.toList
 }
 
-trait Sym {
-  def approx: Double
-}
+trait Sym
 
 trait SymR extends Sym {
-  override lazy val approx = n.toDouble / d.toDouble
-  def n: Int
-  def d: Int
+  def n: SymInt
+  def d: SymInt
 
-  lazy val ratioGcd: Int = n gcd d
+  lazy val ratioGcd: SymInt = n gcd d
 }
 
-case class SymInt(n: Int) extends SymR {
+case class SymInt(int: Int) extends SymR {
   override def toString = n.toString
+  def toInt = int
+  def n = this
   def d = 1
 
-  lazy val primeFactors: SymMultiset[Int] = {
-    var num = n.abs
+  def +(o: SymInt) = SymInt((int: Int) + (o.int: Int))
+  def -(o: SymInt) = SymInt((int: Int) - (o.int: Int))
+  def *(o: SymInt) = SymInt((int: Int) * (o.int: Int))
+  def /(o: SymInt) = SymInt((int: Int) / (o.int: Int))
+  def %(o: SymInt) = SymInt((int: Int) % (o.int: Int))
+  def ^(o: SymInt) = SymInt(math.pow(int, o.int).toInt)
+  def min(o: SymInt) = SymInt(math.min(int, o.int))
+  def max(o: SymInt) = SymInt(math.max(int, o.int))
+  def abs = if (this >= 0) this else SymInt(math.abs(4))
+  def <(o: SymInt) = (int: Int) < (o.int: Int)
+  def >(o: SymInt) = (int: Int) > (o.int: Int)
+  def <=(o: SymInt) = (int: Int) <= (o.int: Int)
+  def >=(o: SymInt) = (int: Int) >= (o.int: Int)
+
+  lazy val primeFactors: SymMultiset[SymInt] = {
+    var num = math.abs(int)
     var f = 2
-    var map = scala.collection.mutable.Map[Int, Int]()
+    var map = scala.collection.mutable.Map[SymInt, SymInt]()
     while (num > 1) {
       var count = 0
       while (num % f == 0) {count += 1 ; num /= f}
-      if (count > 0) map += (f -> count)
+      if (count > 0) map += (SymInt(f) -> SymInt(count))
       f += 1
     }
-    SymMultiset[Int](map.toMap)
+    SymMultiset[SymInt](map.toMap)
   }
 
-  def gcd(o: Int): Int = (this.primeFactors && o.primeFactors).toList
-    .foldLeft(1){(acc, f) => acc * math.pow(f._1, f._2).toInt}
-  def lcm(o: Int): Int = (n * o) / (n gcd o)
+  def gcd(o: SymInt): SymInt = (this.primeFactors && o.primeFactors).toList
+    .foldLeft(SymInt(1)){(acc, f) => acc * (f._1 ^ f._2)}
+  def lcm(o: SymInt): SymInt = (this * o) / (this gcd o)
 }
 
-implicit class ImplicitSymInt(val num: Int) extends SymInt(num)
+implicit class ImplicitSymInt(val original: Int) extends SymInt(original)
 
-case class SymFrac(n: Int, d: Int = 1) extends SymR {
+case class SymFrac(n: SymInt, d: SymInt = 1) extends SymR {
   def gcd(o: SymR): SymR = SymFrac(n gcd o.n, d lcm o.d)
   def lcm(o: SymR): SymR = SymFrac((n * o.d) lcm (o.n * d), d * o.d)
 }
@@ -77,14 +92,8 @@ case class SymUndefined() extends SymR {
   def lcm(o: SymR) = this
 }
 
-case class SymSum(exprs: Sym*) extends Sym {
-  lazy val approx = exprs.map(_.approx).sum
-}
+case class SymSum(exprs: Sym*) extends Sym
 
-case class SymProd(exprs: Sym*) extends Sym {
-  lazy val approx = exprs.map(_.approx).product
-}
+case class SymProd(exprs: Sym*) extends Sym
 
-case class SymPow(base: Sym, pow: Sym) extends Sym {
-  lazy val approx = math.pow(base.approx, pow.approx)
-}
+case class SymPow(base: Sym, pow: Sym) extends Sym
